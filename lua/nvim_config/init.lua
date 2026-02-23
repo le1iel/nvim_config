474eee60d0b9137a4806b8f7a8b2779cfb5d24b3
le1iel <conform2187@proton.me>
2026-02-23 09:51:41 +0000

    neovim 12 rewrite

--- lua/nvim_config/init.lua
+++ lua/nvim_config/init.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 require("nvim_config.keymaps")
 require("nvim_config.options")
-require("nvim_config.lazy")
 require("nvim_config.lsp")
-require("nvim_config.floating_terminal")
+require("nvim_config.plugins")
+require("nvim_config.yank-highlight")
